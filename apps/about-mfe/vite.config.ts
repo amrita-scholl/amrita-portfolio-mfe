f4eb9409717fa5d03d93a4da359181323376cdea
amrita-scholl <amrita.scholl@gmail.com>
2026-08-31 18:50:43 +0530

    add

--- apps/about-mfe/vite.config.ts
+++ apps/about-mfe/vite.config.ts
@@ -1,40 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { federation } from '@module-federation/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-
     federation({
-      name: 'about-mfe',
-
-      filename: 'remoteEntry.js',
-
+      name: "about_mfe",
+      filename: "remoteEntry.js",
       exposes: {
-        './About': './src/About.tsx',
+        "./About": "./src/About.tsx",
       },
-
       shared: {
         react: {
           singleton: true,
         },
-        'react-dom': {
+        "react-dom": {
           singleton: true,
         },
       },
     }),
   ],
-
-  server: {
-    port: 5001,
-  },
-
-  preview: {
-    port: 5001,
-  },
-
-  build: {
-    target: 'esnext',
-  },
 });
