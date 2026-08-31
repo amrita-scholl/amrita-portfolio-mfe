b9685dbc9d385c470e152a70a1eb27ac9cf4462b
amrita-scholl <amrita.scholl@gmail.com>
2026-08-31 19:30:30 +0530

    add

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -13,12 +13,12 @@ export default defineConfig({
         'about-mfe': {
           type: "module",
           name: "about-mfe",
-          entry : 'https://amrita-portfolio-vercel-about-mfe.vercel.app/remoteEntry.js',
+          entry : 'https://amrita-portfolio-mfe-about-mfe.vercel.app/remoteEntry.js',
         },
-        'experience-mfe': { type: 'module', name: 'experience-mfe', entry: 'https://amrita-portfolio-vercel-experience-mfe.vercel.app/remoteEntry.js' },
-        'projects-mfe': { type: 'module', name: 'projects-mfe', entry: 'https://amrita-portfolio-vercel-projects-mfe.vercel.app/remoteEntry.js' },
-        'skills-mfe': { type: 'module', name: 'skills-mfe', entry: 'https://amrita-portfolio-mfe-skills-mfe.vercel.app//remoteEntry.js' },
-        'contact-mfe': { type: 'module', name: 'contact-mfe', entry: 'https://amrita-portfolio-vercel-contact-mfe.vercel.app/remoteEntry.js' },
+        'experience-mfe': { type: 'module', name: 'experience-mfe', entry: 'https://amrita-portfolio-mfe-experience-mfe.vercel.app/remoteEntry.js' },
+        'projects-mfe': { type: 'module', name: 'projects-mfe', entry: 'https://amrita-portfolio-mfe-projects-mfe.vercel.app/remoteEntry.js' },
+        'skills-mfe': { type: 'module', name: 'skills-mfe', entry: 'https://amrita-portfolio-mfe-skills-mfe.vercel.app/remoteEntry.js' },
+        'contact-mfe': { type: 'module', name: 'contact-mfe', entry: 'https://amrita-portfolio-mfe-contact-mfe.vercel.app/remoteEntry.js' },
       },
 
       shared: {  
