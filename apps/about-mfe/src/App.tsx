7f0278ba1c4109388a382c7818d898fbee4454ce
amrita-scholl <amrita.scholl@gmail.com>
2026-09-15 22:02:03 +0530

    add

--- apps/about-mfe/src/App.tsx
+++ apps/about-mfe/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <>
-    
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
